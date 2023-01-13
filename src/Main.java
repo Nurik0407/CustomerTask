07b4c1a69f6c8391e0417f15de557b2ecb290312
Nurik <nuradil936@gmail.com>
2023-01-13 10:39:08 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = {1, 5, 7, 3, 9, 6};
+        for (int i = 0; i < array.length; i++) {
+            array[i] > array[]
+        }
+
+
+    }
+}
